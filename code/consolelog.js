b331240836da11a737d67c231252090426d2fbc4
Kristofer <kris@youngers.org>
2021-12-02 11:23:01 -0500

    checkpoint

---
+++ code/consolelog.js
@@ -0,0 +1,8 @@
+console.log("Hello World!");
+
+console.log(2.25);
+
+let x = 5;
+console.log(x);
+x = x + 2;
+console.log(x);
---
+++ code/variables.js
@@ -0,0 +1,17 @@
+let greeting = 'Hello, World!';
+// define a variable and assign the value 
+
+let user = 'John';
+let age = 25;
+let message = 'Elvis has left the building.';
+
+console.log(message);
+// change the variable
+message = "The tide is high.";
+console.log(message);
+
+let x = 4;
+x = x + 3; //x is now 7
+x = x + 3; //x is now 10
+
+console.log('x is ', x);
